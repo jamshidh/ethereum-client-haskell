a470b97eb866305d8be533ab2fce0fd80ce4f779
jim <jim>
2014-10-17 00:45:21 -0700

    Cleanup.

--- src/Main.hs
+++ src/Main.hs
@@ -269,6 +269,7 @@ main1 = connect "127.0.0.1" "30303" $ \(socket, _) -> do
 
   --sendMessage socket $ Transactions [signedTx]
   maybeBestBlockHash <- withBlockDB getBestBlockHash
+
   bestBlockHash <-
     case maybeBestBlockHash of
       Nothing -> do
@@ -276,39 +277,14 @@ main1 = connect "127.0.0.1" "30303" $ \(socket, _) -> do
         initializeStateDB
         return $ blockHash genesisBlock
       Just x -> return x
+
   putStrLn $ "Best block hash: " ++ format bestBlockHash
+
   sendMessage socket $ GetChain [bestBlockHash] 0x40
   --sendMessage socket $ GetChain [blockHash genesisBlock] 0x40
   putStrLn "Transaction has been sent"
 
   readAndOutput socket
-
-main2::IO ()    
-main2 = do
-  let b = 
-        Block {
-          blockData = BlockData {
-             parentHash = SHA (BigWord {getBigWordInteger = 877290184733011228355131318509245476995638757136170869201259140273173077028}),
-             unclesHash = SHA (BigWord {getBigWordInteger = 13478047122767188135818125966132228187941283477090363246179690878162135454535}),
-             coinbase = Address (BigWord {getBigWordInteger = 521006474229988785287787995209817519331962723518}),
-             stateRoot = SHAPtr $ B.pack $ integer2Bytes 1, transactionsTrie = 0,
-             difficulty = 12917270,
-             number = 0,
-             minGasPrice = 10000000000000,
-             gasLimit = 125000,
-             gasUsed = 0,
-             timestamp = read "2014-09-26 20:27:03 UTC",
-             extraData = 0,
-             nonce = SHA (BigWord {getBigWordInteger = 5})},
-          receiptTransactions = [],
-          blockUncles = []
-          }
-  print b
-  n <- fastFindNonce b
-  print $ showHex n ""
-  let final = headerHashWithoutNonce b `B.append` B.pack (integer2Bytes n)
-  print $ format final
-  print $ format $ C.hash 256 final
 
 -------------------------
 
@@ -318,135 +294,7 @@ main2 = do
 --"/Users/hutong/Library/Application Support/Ethereum/state/"
 --"/tmp/leveldbtest"
 
-startingRoot::B.ByteString
-(startingRoot, "") = B16.decode "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
-                     --"bc36789e7a1e281436464229828f817d6612f7b477d66591ff96a9e064bcc98a"
-                    
-
-genesisRoot::B.ByteString
-(genesisRoot, "") = B16.decode "8dbd704eb38d1c2b73ee4788715ea5828a030650829703f077729b2b613dd206"
-
-stateRoot1::B.ByteString
-(stateRoot1, "") = B16.decode "8b9a53acdafe82b6247c195035995f892f467fda03d7b44b684dcb3663fb3497"
-
-stateRoot2::B.ByteString
-(stateRoot2, "") = B16.decode "b995f323e0811206ec8ed8cadf334a713413eca003c8cbde75a777a647eb6ec7"
-
-stateRoot3::B.ByteString
-(stateRoot3, "") = B16.decode "f22a63cd45e9560b36f1f5cfb95f917601493650d1f5cc237d3f4893ab45f3b9"
-
-stateRoot4::B.ByteString
-(stateRoot4, "") = B16.decode "5893c00aa5e6fcb867d916207fa8cf7339689b9f85aafa92b81574c6e438f887"
-
-
-showStuff::DB.DB->ResourceT IO ()
-showStuff db = do
-    i <- DB.iterOpen db def
-    DB.iterFirst i
-    sequence_ $ replicate 5 $ DB.iterNext i
-    Just key <- DB.iterKey i
-
-    theData1 <- getKeyVals db (SHAPtr key) ""
-    liftIO $ putStrLn $ intercalate "\n" $ kvFormat <$> theData1
-
-    liftIO $ putStrLn "========================"
-    Just genesisData <- getAddressState db (SHAPtr genesisRoot) $ Address 0x51ba59315b3a95761d0863b05ccc7a7f54703d99
-    liftIO $ putStrLn $ format $ genesisData
-
-    liftIO $ putStrLn "========================"
-    Just b1Data <- getAddressState db (SHAPtr stateRoot1) $ Address 0x6ccf6b5c33ae2017a6c76b8791ca61276a69ab8e
-    liftIO $ putStrLn $ format $ b1Data
-
-    liftIO $ putStrLn "========================"
-    Just b2Data <- getAddressState db (SHAPtr stateRoot2) $ Address 0x6ccf6b5c33ae2017a6c76b8791ca61276a69ab8e
-    liftIO $ putStrLn $ format $ b2Data
-
-    liftIO $ putStrLn "========================"
-    Just b3Data <- getAddressState db (SHAPtr stateRoot3) $ Address 0x6ccf6b5c33ae2017a6c76b8791ca61276a69ab8e
-    liftIO $ putStrLn $ format $ b3Data
-
-    liftIO $ putStrLn "========================"
-    Just b4Data <- getAddressState db (SHAPtr stateRoot4) $ Address 0x6ccf6b5c33ae2017a6c76b8791ca61276a69ab8e
-    liftIO $ putStrLn $ format $ b4Data
-
-startingAddressState =
-      AddressState {
-      addressStateNonce=0,
-      balance= 0x0100000000000000000000000000000000000000000000000000,
-      contractRoot=0,
-      codeHash=hash B.empty
-      }
-
-main3::IO ()
-main3 = do
-  let options = DB.defaultOptions {
-        DB.createIfMissing=True, DB.cacheSize=1024}
-  runResourceT $ do
-    db <- DB.open stateDBPath options
-    DB.put db def startingRoot B.empty
-
-    --showStuff db
-
-    let addresses = Address <$> [
-          0x51ba59315b3a95761d0863b05ccc7a7f54703d99,
-          0xe6716f9544a56c530d868e4bfbacb172315bdead,
-          0xb9c015918bdaba24b4ff057a92a3873d6eb201be,
-          0x1a26338f0d905e295fccb71fa9ea849ffa12aaf4,
-          0x2ef47100e0787b915105fd5e3f4ff6752079d5cb,
-          0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826,
-          0x6c386a4b26f73c802f34673f7248bb118f97424a,
-          0xe4157b34ea9615cfbde6b4fda419828124b70c78
-          ]
-
-    newStateRoot <- putAddressStates db (SHAPtr startingRoot) addresses startingAddressState
-
-    let pubKey = prvKey2Address prvKey 
-
-    --let pubKey = Address 0x5b42bd01ff7b368cd80a477cb1cf0d407e2b1cbe
-
-    liftIO $ putStrLn $ "Pub Key: " ++ format pubKey
-    
-    stateRoot2 <- putAddressState db newStateRoot pubKey
-        AddressState {
-          addressStateNonce=0,
-          balance= fromInteger $ 1500*finney,
-          contractRoot=0,
-          codeHash=hash B.empty
-          }
-
-    liftIO $ putStrLn $ "New stateRoot: " ++ format stateRoot2
-
-    --showAllKeyVal db
-
-
-
-
-    liftIO $ putStrLn "========================"
-    liftIO $ putStrLn $ format newStateRoot
-    let SHAPtr x = newStateRoot
-    allData <- getAllAddressStates db newStateRoot
-    liftIO $ putStrLn $ intercalate "\n" $ (\(a, addressState) -> format a ++ tab ("\n" ++ format addressState)) <$> allData
-
-
-
-    return ()
-
-putAddressStates::DB.DB->SHAPtr->[Address]->AddressState->ResourceT IO SHAPtr
-putAddressStates _ stateRoot [] _ = return stateRoot
-putAddressStates db stateRoot (address:rest) addressState = do
-    newStateRoot <- putAddressState db stateRoot address addressState
-    putAddressStates db newStateRoot rest addressState
-
-  {-
-s_ret[Address(fromHex(i))] = 
-AddressState(0, u256(1) << 200, h256(), EmptySHA3);
-
-putKeyVal "1a26338f0d905e295fccb71fa9ea849ffa12aaf4" [0x0, 0x0100000000000000000000000000000000000000000000000000, 0x0, 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470]
--}
-
 
 main::IO ()    
 main = do
-  --main3
-  --main2
   main1
