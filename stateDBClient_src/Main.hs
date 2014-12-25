383e07cc148f0f61c6bc0fd7fb2774e7cb47907b
jamshid <j>
2014-12-25 12:35:29 -0500

    I am turning the stateDBClient into a general purpose DB querying tool to get various types of values from the Ethereum DB.

--- stateDBClient_src/Main.hs
+++ stateDBClient_src/Main.hs
@@ -1,49 +1,86 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable, OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall #-}
 
-import Control.Monad.IO.Class
-import Control.Monad.State
-import qualified Data.ByteString as B
+import System.Console.CmdArgs
+
+import State
+import Block
+import Init
+import Code
+
+--import Debug.Trace
+
+
+
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8 as BC
-import Data.Functor
-import Data.List
-import qualified Database.LevelDB as DB
-import System.Environment
-import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
+--import Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 
-import qualified Data.NibbleString as N
-import Data.RLP
-
-import Context
 import ExtDBs
-import Format
 
-formatKV::(N.NibbleString, RLPObject)->Doc
-formatKV (key, val) =
-    pretty key <> text ": " <> pretty (rlpDeserialize $ rlpDecode val)
 
-doit::SHAPtr->ContextM ()
-doit sr = do
-    setStateRoot sr
-    kvs <- getKeyVals ""
-    --liftIO $ putStrLn $ displayS (renderPretty 1.0 200 $ vsep $ formatKV <$> kvs) ""
-    liftIO $ putStrLn $ displayS (renderPretty 1.0 200 $ vsep $ formatKV <$> filter (filterUnnecessary . fst) kvs) ""
+data Options = 
+  State{root::String, db::String} 
+  | Block{hash::String, db::String} 
+  | Code{hash::String, db::String}
+  | Init{hash::String, db::String}
+  deriving (Show, Data, Typeable)
 
+stateOptions::Annotate Ann
+stateOptions = 
+  record State{root=undefined, db=undefined} [
+    root := def += typ "SHAPtr" += argPos 1,
+    db := def += typ "DBSTRING" += argPos 0
+    ]
+
+blockOptions::Annotate Ann
+blockOptions = 
+  record Block{hash=undefined, db=undefined} [
+    hash := def += typ "FILENAME" += argPos 1 += opt ("-"::String),
+    db := def += typ "DBSTRING" += argPos 0
+    ]
+
+initOptions::Annotate Ann
+initOptions = 
+  record Init{hash=undefined, db=undefined} [
+    hash := def += typ "FILENAME" += argPos 1 += opt ("-"::String),
+    db := def += typ "DBSTRING" += argPos 0
+    ]
+
+codeOptions::Annotate Ann
+codeOptions = 
+  record Code{hash=undefined, db=undefined} [
+    hash := def += typ "USERAGENT" += argPos 1,
+    db := def += typ "DBSTRING" += argPos 0
+    ]
+
+options::Annotate Ann
+options = modes_ [stateOptions, blockOptions, initOptions, codeOptions]
+
+
+--      += summary "Apply shims, reorganize, and generate to the input"
+
+
+main::IO ()
 main = do
-  [theType, addr] <- getArgs
-  let sr = SHAPtr $ fst $ B16.decode $ BC.pack addr
-  DB.runResourceT $ do
-    cxt <- openDBs theType
-    _ <- liftIO $ runStateT (doit sr) cxt
-    return ()
+  opts <- cmdArgs_ options
+  run opts
+    
+-------------------
 
-filterUnnecessary::N.NibbleString->Bool
-filterUnnecessary "1a26338f0d905e295fccb71fa9ea849ffa12aaf4" = False
-filterUnnecessary "2ef47100e0787b915105fd5e3f4ff6752079d5cb" = False
-filterUnnecessary "51ba59315b3a95761d0863b05ccc7a7f54703d99" = False
-filterUnnecessary "6c386a4b26f73c802f34673f7248bb118f97424a" = False
-filterUnnecessary "b9c015918bdaba24b4ff057a92a3873d6eb201be" = False
-filterUnnecessary "cd2a3d9f938e13cd947ec05abc7fe734df8dd826" = False
-filterUnnecessary "e4157b34ea9615cfbde6b4fda419828124b70c78" = False
-filterUnnecessary "e6716f9544a56c530d868e4bfbacb172315bdead" = False
-filterUnnecessary _ = True
+
+run::Options->IO ()
+
+run State{root=r, db=db'} = do
+  let sr = SHAPtr $ fst $ B16.decode $ BC.pack r
+  State.doit db' sr
+
+run Block{hash=h, db=db'} = do
+  Block.doit db' h
+
+run Init{hash=h, db=db'} = do
+  Init.doit db' h
+
+run Code{hash=h, db=db'} = do
+  Code.doit db' h
+
