a0f3781956b18a7c72be198e7b3d801759e0c3f4
jim <jim>
2015-04-06 15:13:09 -0700

    Cleanup

--- exec_src/Main.hs
+++ exec_src/Main.hs
@@ -46,32 +46,13 @@ import Blockchain.Util
 --import Debug.Trace
 
 
-
-
-
-
-
-
-
-
-
-
-
-import Control.Monad.IO.Class
 import Crypto.PubKey.ECC.DH
 import Crypto.Types.PubKey.ECC
 import Crypto.Random
-import qualified Data.ByteString as B
---import qualified Data.ByteString.Base16 as B16
---import qualified Data.ByteString.Char8 as BC
 import Data.Maybe
-import qualified Network.Haskoin.Internals as H
---import Numeric
+--import qualified Network.Haskoin.Internals as H
 
 import Blockchain.Format
-import Blockchain.Data.RLP
-import Blockchain.Data.Wire
-import Blockchain.SHA (SHA(..))
 
 import Blockchain.Frame
 import Blockchain.UDP
@@ -86,18 +67,8 @@ import Blockchain.RLPx
 
 
 
-
-
-
-
-
-
 prvKey::PrvKey
 Just prvKey = makePrvKey 0xac3e8ce2ef31c3f45d5da860bcd9aee4b37a05c5a3ddee40dd061620c3dab380
---Just prvKey = makePrvKey 0xd69bceff85f3bc2d0a13bcc43b7caf6bd54a21ad0c1997ae623739216710ca19 --cpp client prvKey
---6ccf6b5c33ae2017a6c76b8791ca61276a69ab8e --cpp coinbase
-
-
 
 getNextBlock::Block->UTCTime->ContextM Block
 getNextBlock b ts = do
@@ -208,22 +179,13 @@ readAndOutput = do
   handleMsg msg
   readAndOutput
   
-{-
-handleAllPayloads $ getPayloads $ BL.unpack payloads
-  where
-    handleAllPayloads [] = error "Server has closed the connection"
-    handleAllPayloads (pl:rest) = do
-      handlePayload $ B.pack pl
-      handleAllPayloads rest
--}
-
 mkHello::IO Message
 mkHello = do
   peerId <- getEntropy 64
   return Hello {
                version = 3,
                clientId = "Ethereum(G)/v0.6.4//linux/Haskell",
-               capability = [ETH ethVersion, SHH shhVersion],
+               capability = [ETH ethVersion], -- , SHH shhVersion],
                port = 30303,
                nodeId = fromIntegral $ byteString2Integer peerId
              }
@@ -241,8 +203,8 @@ createTransactions transactions = do
       liftIO $ withSource devURandom $ signTransaction prvKey t{tNonce=n}
 -}
 
-doit2::EthCryptM ContextM ()
-doit2 = do
+doit::EthCryptM ContextM ()
+doit = do
     liftIO $ putStrLn "Connected"
 
     lift $ lift $ addCode B.empty --This is probably a bad place to do this, but I can't think of a more natural place to do it....  Empty code is used all over the place, and it needs to be in the database.
@@ -272,8 +234,19 @@ doit2 = do
 
     readAndOutput
 
-doit::String->PortNumber->ContextM ()
-doit ipAddress thePort = do
+
+
+main::IO ()    
+main = do
+  args <- getArgs
+
+  let serverNum =
+        case args of
+          (arg:_) -> arg
+          [] -> "1" --Just default to poc-8.ethdev.com
+
+  let (ipAddress, thePort) = ipAddresses !! read serverNum
+
   entropyPool <- liftIO createEntropyPool
 
   let g = cprgCreate entropyPool :: SystemRNG
@@ -281,69 +254,12 @@ doit ipAddress thePort = do
 
   liftIO $ putStrLn $ "Attempting to connect to " ++ show ipAddress ++ ":" ++ show thePort
   
-  otherPubKey <- liftIO $ fmap hPubKeyToPubKey $ getServerPubKey ipAddress thePort
-
-  runEthCryptM myPriv otherPubKey ipAddress (fromIntegral thePort) doit2
-
-
-
---I need to use two definitions of PubKey (internally they represent the same thing)
---The one in the Haskoin package allows me to recover signatures.
---The one in the crypto packages let me do AES encryption.
---At some point I have to convert from one PubKey to the other, this function
---lets me to that.
-hPubKeyToPubKey::H.PubKey->Point
-hPubKeyToPubKey (H.PubKey hPoint) =
-  Point (fromIntegral x) (fromIntegral y)
-  where
-    x = fromMaybe (error "getX failed in prvKey2Address") $ H.getX hPoint
-    y = fromMaybe (error "getY failed in prvKey2Address") $ H.getY hPoint
-hPubKeyToPubKey (H.PubKeyU _) = error "PubKeyU not supported in hPubKeyToPUbKey yet"
-
-main::IO ()    
-main = do
-  args <- getArgs
-
-  let ipNum =
-        case args of
-          (arg:_) -> arg
-          [] -> "1" --Just default to poc-8.ethdev.com
-
-  let (ipAddress, thePort) = ipAddresses !! read ipNum
-
-  putStrLn $ "Attempting to connect to " ++ show ipAddress ++ ":" ++ show thePort
-  putStrLn $ "Attempting to connect to " ++ show ipAddress ++ ":" ++ show ipNum
+  otherPubKey <- liftIO $ getServerPubKey ipAddress thePort
 
   runResourceT $ do
       cxt <- openDBs "h"
-      _ <- runStateT (runStateT (doit ipAddress thePort) (Context [] 0 [] False)) cxt
+      _ <- flip runStateT cxt $
+           flip runStateT (Context [] 0 [] False) $
+           runEthCryptM myPriv otherPubKey ipAddress (fromIntegral thePort) doit
       return ()
 
-{-
-    sendMsg Hello {
-      version=3,
-      clientId="dummyClient",
-      capability=[ETH 60],
-      port=30303,
-      nodeId=0x1
-      }
-    liftIO . putStrLn . format =<< recvMsg
-    sendMsg Ping
-    liftIO . putStrLn . format =<< recvMsg
-    sendMsg Pong
-    liftIO . putStrLn . format =<< recvMsg
-    sendMsg Status{
-      protocolVersion=60,
-      networkID="",
-      totalDifficulty=131072,
-      latestHash=SHA 0,
-      genesisHash=SHA 0xfd4af92a79c7fc2fd8bf0d342f2e832e1d4f485c85b9152d2039e03bc604fdca
-      }
-    liftIO . putStrLn . format =<< recvMsg
-    liftIO . putStrLn . format =<< recvMsg
-    liftIO . putStrLn . format =<< recvMsg
-
-
-  return ()
-
--}
