c7c6c94fed6b6329b28b1d577e971cb7497f0c2f
jim <jim>
2015-07-26 21:27:08 -0700

    Reorganizing BlockChain before the break apart.

--- src/Blockchain/BlockChain.hs
+++ src/Blockchain/BlockChain.hs
@@ -54,22 +54,6 @@ import Blockchain.VM.VMState
 
 --import Debug.Trace
 
-runCodeForTransaction::Block->Integer->Address->Address->Transaction->ContextM (Either VMException B.ByteString, VMState)
-runCodeForTransaction b availableGas tAddr newAddress ut | isContractCreationTX ut = do
-  when flags_debug $ liftIO $ putStrLn "runCodeForTransaction: ContractCreationTX"
-
-  (result, vmState) <-
-    create b 0 tAddr tAddr (transactionValue ut) (transactionGasPrice ut) availableGas newAddress (transactionInit ut)
-
-  return (const B.empty <$> result, vmState)
-
-runCodeForTransaction b availableGas tAddr owner ut = do --MessageTX
-  when flags_debug $ liftIO $ putStrLn $ "runCodeForTransaction: MessageTX caller: " ++ show (pretty $ tAddr) ++ ", address: " ++ show (pretty $ transactionTo ut)
-
-  call b 0 owner owner tAddr
-          (fromIntegral $ transactionValue ut) (fromIntegral $ transactionGasPrice ut)
-          (transactionData ut) (fromIntegral availableGas) tAddr
-
 addBlocks::Bool->[Block]->ContextM ()
 addBlocks isBeingCreated blocks = 
   forM_ blocks $ \block -> do
@@ -79,35 +63,115 @@ addBlocks isBeingCreated blocks =
 
     liftIO $ putStrLn $ "#### Block insertion time = " ++ printf "%.4f" (realToFrac $ after - before::Double) ++ "s"
 
-codeOrDataLength::Transaction->Int
-codeOrDataLength t | isMessageTX t = B.length $ transactionData t
-codeOrDataLength t = codeLength $ transactionInit t --is ContractCreationTX
+addBlock::Bool->Block->ContextM ()
+addBlock isBeingCreated b@Block{blockBlockData=bd, blockBlockUncles=uncles} = do
+  liftIO $ putStrLn $ "Inserting block #" ++ show (blockDataNumber bd) ++ " (" ++ show (pretty $ blockHash b) ++ ")."
+  maybeParent <- getBlock $ blockDataParentHash bd
+  case maybeParent of
+    Nothing ->
+      liftIO $ putStrLn $ "Missing parent block in addBlock: " ++ show (pretty $ blockDataParentHash bd) ++ "\n" ++
+      "Block will not be added now, but will be requested and added later"
+    Just parentBlock -> do
+      setStateDBStateRoot $ blockDataStateRoot $ blockBlockData parentBlock
+      let rewardBase = 1500 * finney
+      s1 <- addToBalance (blockDataCoinbase bd) rewardBase
+      when (not s1) $ error "addToBalance failed even after a check in addBlock"
 
-zeroBytesLength::Transaction->Int
-zeroBytesLength t | isMessageTX t = length $ filter (==0) $ B.unpack $ transactionData t
-zeroBytesLength t = length $ filter (==0) $ B.unpack codeBytes' --is ContractCreationTX
-                  where
-                    Code codeBytes' = transactionInit t
+      forM_ uncles $ \uncle -> do
+        s2 <- addToBalance (blockDataCoinbase bd) (rewardBase `quot` 32)
+        when (not s2) $ error "addToBalance failed even after a check in addBlock"
+        
+        s3 <- addToBalance
+              (blockDataCoinbase uncle)
+              ((rewardBase*(8+blockDataNumber uncle - blockDataNumber bd )) `quot` 8)
+        when (not s3) $ error "addToBalance failed even after a check in addBlock"
 
-intrinsicGas::Transaction->Integer
-intrinsicGas t = gTXDATAZERO * zeroLen + gTXDATANONZERO * (fromIntegral (codeOrDataLength t) - zeroLen) + gTX
-    where
-      zeroLen = fromIntegral $ zeroBytesLength t
---intrinsicGas t@ContractCreationTX{} = 5 * (fromIntegral (codeOrDataLength t)) + 500
+
+      let transactions = blockReceiptTransactions b
+
+      addTransactions b (blockDataGasLimit $ blockBlockData b) transactions
+
+      db <- getStateDB
+
+      b' <-
+        if isBeingCreated
+        then return b{blockBlockData = (blockBlockData b){blockDataStateRoot=MP.stateRoot db}}
+        else do
+          when ((blockDataStateRoot (blockBlockData b) /= MP.stateRoot db)) $ do
+            liftIO $ putStrLn $ "newStateRoot: " ++ show (pretty $ MP.stateRoot db)
+            error $ "stateRoot mismatch!!  New stateRoot doesn't match block stateRoot: " ++ show (pretty $ blockDataStateRoot $ blockBlockData b)
+          return b
+
+      valid <- checkValidity b'
+      case valid of
+        Right () -> return ()
+        Left err -> error err
+      -- let bytes = rlpSerialize $ rlpEncode b
+      blkDataId <- putBlock b'
+      replaceBestIfBetter (blkDataId, b')
+
+addTransactions::Block->Integer->[Transaction]->ContextM ()
+addTransactions _ _ [] = return ()
+addTransactions b blockGas (t:rest) = do
+  printTransactionMessage t
+
+  before <- liftIO $ getPOSIXTime 
+
+  stateRootBefore <- fmap MP.stateRoot getStateDB
+
+  result <- runEitherT $ addTransaction b blockGas t
+
+  let (resultString, response) =
+          case result of 
+            Left err -> (err, "")
+            Right (state', _) -> ("Success!", BC.unpack $ B16.encode $ fromMaybe "" $ returnVal state')
+
+  after <- liftIO $ getPOSIXTime 
+
+  stateRootAfter <- fmap MP.stateRoot getStateDB
+
+  mpdb <- getStateDB
+
+  addrDiff <- addrDbDiff mpdb stateRootBefore stateRootAfter
+
+  detailsString <- getDebugMsg
+  _ <-
+    putTransactionResult $
+    TransactionResult {
+      transactionResultBlockHash=blockHash b,
+      transactionResultTransactionHash=transactionHash t,
+      transactionResultMessage=resultString,
+      transactionResultResponse=response,
+      transactionResultTrace=detailsString,
+      transactionResultGasUsed=0,
+      transactionResultEtherUsed=0,
+      transactionResultContractsCreated=intercalate "," $ map formatAddress [x|CreateAddr x _ <- addrDiff],
+      transactionResultContractsDeleted=intercalate "," $ map formatAddress [x|DeleteAddr x <- addrDiff],
+      transactionResultTime=realToFrac $ after - before::Double,
+      transactionResultNewStorage="",
+      transactionResultDeletedStorage=""
+      }
+  
+
+  clearDebugMsg
+
+  liftIO $ putStrLn $ CL.magenta "    |" ++ " t = " ++ printf "%.2f" (realToFrac $ after - before::Double) ++ "s                                                              " ++ CL.magenta "|"
+  liftIO $ putStrLn $ CL.magenta "    =========================================================================="
+
+  remainingBlockGas <-
+    case result of
+      Left e -> do
+        liftIO $ putStrLn $ CL.red "Insertion of transaction failed!  " ++ e
+        return blockGas
+      Right (_, g') -> return g'
+
+  addTransactions b remainingBlockGas rest
 
 addTransaction::Block->Integer->Transaction->EitherT String ContextM (VMState, Integer)
 addTransaction b remainingBlockGas t = do
-  let maybeAddr = whoSignedThisTransaction t
+  tAddr <- whoSignedThisTransaction t ?! "malformed signature"
 
-  case maybeAddr of
-    Just _ -> return ()
-    Nothing -> left "malformed signature"
-
-  let Just tAddr = maybeAddr
-    
   nonceValid <- lift $ isNonceValid t
-
-
 
   let intrinsicGas' = intrinsicGas t
   when flags_debug $
@@ -167,6 +231,41 @@ addTransaction b remainingBlockGas t = do
         liftIO $ putStrLn $ "Insufficient funds to run the VM: need " ++ show (availableGas*transactionGasPrice t) ++ ", have " ++ show (addressStateBalance addressState')
         return (VMState{vmException=Just InsufficientFunds, vmGasRemaining=0, refund=0, debugCallCreates=Nothing, suicideList=[], logs=[], returnVal=Nothing}, remainingBlockGas)
 
+runCodeForTransaction::Block->Integer->Address->Address->Transaction->ContextM (Either VMException B.ByteString, VMState)
+runCodeForTransaction b availableGas tAddr newAddress ut | isContractCreationTX ut = do
+  when flags_debug $ liftIO $ putStrLn "runCodeForTransaction: ContractCreationTX"
+
+  (result, vmState) <-
+    create b 0 tAddr tAddr (transactionValue ut) (transactionGasPrice ut) availableGas newAddress (transactionInit ut)
+
+  return (const B.empty <$> result, vmState)
+
+runCodeForTransaction b availableGas tAddr owner ut = do --MessageTX
+  when flags_debug $ liftIO $ putStrLn $ "runCodeForTransaction: MessageTX caller: " ++ show (pretty $ tAddr) ++ ", address: " ++ show (pretty $ transactionTo ut)
+
+  call b 0 owner owner tAddr
+          (fromIntegral $ transactionValue ut) (fromIntegral $ transactionGasPrice ut)
+          (transactionData ut) (fromIntegral availableGas) tAddr
+
+----------------
+
+
+codeOrDataLength::Transaction->Int
+codeOrDataLength t | isMessageTX t = B.length $ transactionData t
+codeOrDataLength t = codeLength $ transactionInit t --is ContractCreationTX
+
+zeroBytesLength::Transaction->Int
+zeroBytesLength t | isMessageTX t = length $ filter (==0) $ B.unpack $ transactionData t
+zeroBytesLength t = length $ filter (==0) $ B.unpack codeBytes' --is ContractCreationTX
+                  where
+                    Code codeBytes' = transactionInit t
+
+intrinsicGas::Transaction->Integer
+intrinsicGas t = gTXDATAZERO * zeroLen + gTXDATANONZERO * (fromIntegral (codeOrDataLength t) - zeroLen) + gTX
+    where
+      zeroLen = fromIntegral $ zeroBytesLength t
+--intrinsicGas t@ContractCreationTX{} = 5 * (fromIntegral (codeOrDataLength t)) + 500
+
 
 printTransactionMessage::Transaction->ContextM ()
 printTransactionMessage t = do
@@ -186,109 +285,16 @@ printTransactionMessage t = do
 formatAddress::Address->String
 formatAddress (Address x) = BC.unpack $ B16.encode $ B.pack $ word160ToBytes x
 
-addTransactions::Block->Integer->[Transaction]->ContextM ()
-addTransactions _ _ [] = return ()
-addTransactions b blockGas (t:rest) = do
-  printTransactionMessage t
-
-  before <- liftIO $ getPOSIXTime 
-
-  stateRootBefore <- fmap MP.stateRoot getStateDB
-
-  result <- runEitherT $ addTransaction b blockGas t
-
-  let (resultString, response) =
-          case result of 
-            Left err -> (err, "")
-            Right (state', _) -> ("Success!", BC.unpack $ B16.encode $ fromMaybe "" $ returnVal state')
-
-  after <- liftIO $ getPOSIXTime 
-
-  stateRootAfter <- fmap MP.stateRoot getStateDB
-
-  mpdb <- getStateDB
-
-  addrDiff <- addrDbDiff mpdb stateRootBefore stateRootAfter
-
-  detailsString <- getDebugMsg
-  _ <-
-    putTransactionResult $
-    TransactionResult {
-      transactionResultBlockHash=blockHash b,
-      transactionResultTransactionHash=transactionHash t,
-      transactionResultMessage=resultString,
-      transactionResultResponse=response,
-      transactionResultTrace=detailsString,
-      transactionResultGasUsed=0,
-      transactionResultEtherUsed=0,
-      transactionResultContractsCreated=intercalate "," $ map formatAddress [x|CreateAddr x _ <- addrDiff],
-      transactionResultContractsDeleted=intercalate "," $ map formatAddress [x|DeleteAddr x <- addrDiff],
-      transactionResultTime=realToFrac $ after - before::Double,
-      transactionResultNewStorage="",
-      transactionResultDeletedStorage=""
-      }
-  
-
-  clearDebugMsg
-
-  liftIO $ putStrLn $ CL.magenta "    |" ++ " t = " ++ printf "%.2f" (realToFrac $ after - before::Double) ++ "s                                                              " ++ CL.magenta "|"
-  liftIO $ putStrLn $ CL.magenta "    =========================================================================="
-
-  remainingBlockGas <-
-    case result of
-      Left e -> do
-        liftIO $ putStrLn $ CL.red "Insertion of transaction failed!  " ++ e
-        return blockGas
-      Right (_, g') -> return g'
-
-  addTransactions b remainingBlockGas rest
-  
-addBlock::Bool->Block->ContextM ()
-addBlock isBeingCreated b@Block{blockBlockData=bd, blockBlockUncles=uncles} = do
-  liftIO $ putStrLn $ "Inserting block #" ++ show (blockDataNumber bd) ++ " (" ++ show (pretty $ blockHash b) ++ ")."
-  maybeParent <- getBlock $ blockDataParentHash bd
-  case maybeParent of
-    Nothing ->
-      liftIO $ putStrLn $ "Missing parent block in addBlock: " ++ show (pretty $ blockDataParentHash bd) ++ "\n" ++
-      "Block will not be added now, but will be requested and added later"
-    Just parentBlock -> do
-      setStateDBStateRoot $ blockDataStateRoot $ blockBlockData parentBlock
-      let rewardBase = 1500 * finney
-      s1 <- addToBalance (blockDataCoinbase bd) rewardBase
-      when (not s1) $ error "addToBalance failed even after a check in addBlock"
-
-      forM_ uncles $ \uncle -> do
-        s2 <- addToBalance (blockDataCoinbase bd) (rewardBase `quot` 32)
-        when (not s2) $ error "addToBalance failed even after a check in addBlock"
-        
-        s3 <- addToBalance
-              (blockDataCoinbase uncle)
-              ((rewardBase*(8+blockDataNumber uncle - blockDataNumber bd )) `quot` 8)
-        when (not s3) $ error "addToBalance failed even after a check in addBlock"
+--Convert Maybe exception handling to EitherT exception
+(?!)::Monad m=>
+      Maybe a->err->EitherT err m a
+x ?! err = maybe (left err) return $ x
 
 
-      let transactions = blockReceiptTransactions b
 
-      addTransactions b (blockDataGasLimit $ blockBlockData b) transactions
 
-      db <- getStateDB
 
-      b' <-
-        if isBeingCreated
-        then return b{blockBlockData = (blockBlockData b){blockDataStateRoot=MP.stateRoot db}}
-        else do
-          when ((blockDataStateRoot (blockBlockData b) /= MP.stateRoot db)) $ do
-            liftIO $ putStrLn $ "newStateRoot: " ++ show (pretty $ MP.stateRoot db)
-            error $ "stateRoot mismatch!!  New stateRoot doesn't match block stateRoot: " ++ show (pretty $ blockDataStateRoot $ blockBlockData b)
-          return b
-
-      valid <- checkValidity b'
-      case valid of
-        Right () -> return ()
-        Left err -> error err
-      -- let bytes = rlpSerialize $ rlpEncode b
-      blkDataId <- putBlock b'
-      replaceBestIfBetter (blkDataId, b')
+----------------
 
 getBestBlockHash::ContextM SHA
 getBestBlockHash = do
